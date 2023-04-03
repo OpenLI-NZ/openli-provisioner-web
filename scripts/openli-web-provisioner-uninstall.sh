91ea4697207c347c3c261813fbd965ad19bfaf37
Shane Alcock <shane@alcock.co.nz>
2023-04-03 18:40:06 +1200

    Don't remove nvm / nodejs on uninstall
    The user might be using them for something else that they added
    after we were installed, so instead let's just tell them how
    to remove nvm and nodejs themselves if they want to rather than
    potentially making a bad decision for them...

--- scripts/openli-web-provisioner-uninstall.sh
+++ scripts/openli-web-provisioner-uninstall.sh
@@ -19,13 +19,15 @@ if [ -d ${SPACE}/node_modules/\@openli/openli-provisioner-web ]; then
     make prefix=/usr/local uninstall
 fi
 
-if [ -f ${SPACE}/.nvm-installed ]; then
-    nvm deactivate
-    nvm unload
-    sed -i '/NVM_DIR/d' ${HOME}/.bashrc
-fi
-
 if [ -d ${SPACE} ]; then
     rm -r ${SPACE}
 fi
 
+echo
+echo "OpenLI provisioner web has been uninstalled, but we've left nvm"
+echo "and nodejs on your system in case something else is using them."
+echo
+echo "If you don't need them anymore and want to clean them up, please"
+echo "run the following: "
+echo "    nvm deactivate; nvm unload; sed -i '/NVM_DIR/d' ${HOME}/.bashrc"
+echo
