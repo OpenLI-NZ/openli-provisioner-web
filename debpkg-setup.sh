70a2d28846720ae2e34b937e7a423415d7a24639
Shane Alcock <shane@alcock.co.nz>
2023-04-03 18:44:29 +1200

    Add debian packaging setup scripts to repo
    Based off the ones for OpenLI, minus all the stuff that we don't
    need here.

---
+++ debpkg-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x -e -o pipefail
+
+export DEBEMAIL='shane@alcock.co.nz'
+export DEBFULLNAME='Shane Alcock'
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
+    apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git \
+    pkg-config sed
+
+DISTRO=$(lsb_release -sc)
+
+case ${DISTRO} in
+        bullseye )
+                sed -i 's/ dh-systemd (>=1.5),//' debian/control
+        ;;
+
+        jammy )
+                sed -i 's/ dh-systemd (>=1.5),//' debian/control
+        ;;
+
+        bionic )
+                apt-get install -y debhelper -t bionic-backports
+        ;;
+esac
+
+
+apt-get update
+apt-get upgrade -y
