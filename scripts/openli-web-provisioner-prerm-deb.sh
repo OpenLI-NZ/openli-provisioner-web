eec3493f92ef6da2c9a8a4b581a26360e72a0958
Shane Alcock <shane@alcock.co.nz>
2024-03-13 17:09:55 +1300

    deb: remove leftover __pycache__ directory from /etc/ on uninstall

--- scripts/openli-web-provisioner-prerm-deb.sh
+++ scripts/openli-web-provisioner-prerm-deb.sh
@@ -10,6 +10,12 @@ systemctl daemon-reload
 
 SPACE=/usr/local/src/openli-provisioner-web
 
+# gunicorn can create a __pycache__ directory that apt won't
+# remove on its own
+if [ -d /etc/openli-provisioner-web/__pycache__ ]; then
+    rm /etc/openli-provisioner-web/__pycache__
+fi
+
 if [ -d ${SPACE}/node_modules/\@openli/openli-provisioner-web ]; then
     cd ${SPACE}/node_modules/\@openli/openli-provisioner-web
     make prefix=/usr/local uninstall
